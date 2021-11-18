3baeb9cbfbd921419045d8f4caa3abeaf9e27811
uyalexandre <uy.alexandre@yahoo.com>
2021-11-18 17:00:23 +0100

    test AppEngine

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const express = require('express');
+
+var bodyParser = require('body-parser')
+
+const app = express();
+
+const jsonParser = bodyParser.json();
+
+const server = http.createServer((req, res) => {
+    res.end('Hello World !');
+});
+
+server.listen(process.env.PORT || 3000);
